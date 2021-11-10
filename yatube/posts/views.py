1254870ec6211875c927f5dcac2733f68e5b498d
NewStarVitaliy <pasha15wot333@mail.ru>
2021-11-10 20:45:47 +0500

    delete all dcstr, and append migrate

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,7 @@ from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:10]
     title = 'Это главная страница проекта Yatube'
     context = {
         'title': title,
@@ -16,7 +16,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     title = f'Записи сообщества {group}'
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).all()[:10]
     context = {
         'title': title,
         'group': group,
